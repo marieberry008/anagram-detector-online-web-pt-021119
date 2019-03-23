90dbf65965d74ffdebfe9a695f97bdaf1c99aa88
Marie Berry (Leugermann) <marie@creativeos.io>
2019-03-23 21:50:28 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -5,9 +5,9 @@ class Anagram
     @check_word = check_word
   end 
   
-  def match(find_anagram_in_this_string) 
-    puts "Must find #{self.check_word} inside the following string: #{find_anagram_in_this_string}"
-    find_anagram_in_this_string.find_all do |word| 
+  def match(find_anagram_in_string) 
+    puts "Must find #{self.check_word} inside the string: #{find_anagram_in_string}"
+    find_anagram_in_string.find_all do |word| 
       if word.split("").sort == self.check_word.split("").sort 
         word 
       end
@@ -17,5 +17,3 @@ class Anagram
 end 
 
 
-listen = Anagram.new("listen")
-listen.match(%w(enlists google inlets inlest banana))
