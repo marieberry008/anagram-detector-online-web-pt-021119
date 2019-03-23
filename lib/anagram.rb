0f2ae2ab68a269da8639c5c367cdc72f43b9b442
Marie Berry (Leugermann) <marie@creativeos.io>
2019-03-23 21:48:13 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,14 +1,14 @@
 class Anagram
   attr_accessor :check_word
   
-  def initialize(word_to_analyze)
-    @word_to_analyze = word_to_analyze 
+  def initialize(check_word)
+    @check_word = check_word
   end 
   
   def match(find_anagram_in_this_string) 
-    puts "Must find #{self.word_to_analyze} inside the following string: #{find_anagram_in_this_string}"
+    puts "Must find #{self.check_word} inside the following string: #{find_anagram_in_this_string}"
     find_anagram_in_this_string.find_all do |word| 
-      if word.split("").sort == self.word_to_analyze.split("").sort 
+      if word.split("").sort == self.check_word.split("").sort 
         word 
       end
     end 
