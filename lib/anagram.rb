f1836ac7fdac03cfa1e2c30128f3b94a4e0230b2
Marie Berry (Leugermann) <marie@creativeos.io>
2019-03-23 21:52:06 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :check_word
+  
+  def initialize(check_word)
+    @check_word = check_word
+  end 
+  
+  def match(find_anagram_in_string) 
+    puts "Must find #{self.check_word} inside the string: #{find_anagram_in_string}"
+    find_anagram_in_string.find_all do |word| 
+      if word.split("").sort == self.check_word.split("").sort 
+        word 
+      end
+    end 
+  end
+end 
+
+
